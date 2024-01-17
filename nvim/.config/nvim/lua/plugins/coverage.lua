5c02c59e388b74432d04cfbd8a974c5ccb366f44
Tholo <ali.mohamadza@gmail.com>
2024-01-17 21:35:21 +0330

    coverage

--- nvim/.config/nvim/lua/plugins/coverage.lua
+++ nvim/.config/nvim/lua/plugins/coverage.lua
@@ -1,3 +1,37 @@
 return {
   "andythigpen/nvim-coverage",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    {
+      "<leader>Cl",
+      function()
+        require("coverage").load({ true })
+      end,
+      desc = "Code Coverage Load",
+    },
+    {
+      "<leader>Cc",
+      function()
+        require("coverage").toggle()
+      end,
+      desc = "Code Coverage Toggle",
+    },
+    {
+      "<leader>Cs",
+      function()
+        require("coverage").summary()
+      end,
+      desc = "Code Coverage Summary",
+    },
+  },
+  config = function()
+    require("coverage").setup({
+      auto_reload = true,
+      highlights = {
+        uncovered = { fg = "#d62929" },
+      },
+    })
+  end,
 }
